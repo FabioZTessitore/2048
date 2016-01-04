9c895b6a68d62b061d13440a2a9f4bf063765752
Fabio Z Tessitore <fabioztessitore@gmail.com>
2016-01-05 00:42:08 +0100

    aggiunti commenti a random.h

--- random.h
+++ random.h
@@ -1,7 +1,26 @@
 #ifndef RANDOM_H
 #define RANDOM_H
 
+/* random
+ *
+ * wrapper attorno alle funzioni
+ * per la generazione di numeri pseudo-casuali
+ */
+
+
+/* random_init:
+ *
+ * inizializza il sistema di generazione
+ * dei numeri pseudo-casuali
+ */
 void random_init();
+
+
+/* random_between:
+ *
+ * genera e restituisce un numero
+ * compreso tra min incluso e max escluso
+ */
 int random_between(int min, int max);
 
 #endif
