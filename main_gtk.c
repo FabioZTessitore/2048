87c5d4d153bccd3ad6d8f52b6e26c8ba2b74bfd3
Fabio Z Tessitore <fabioztessitore@gmail.com>
2016-01-08 00:34:00 +0100

    prima versione gtk

--- main_gtk.c
+++ main_gtk.c
@@ -1,45 +1,117 @@
 #include <gtk/gtk.h>
+#include "board.h"
+
+Board game_board;
+GtkWidget *window;
+GtkWidget *board_container = NULL;
+
+void create_board_grid();
+
+static gint
+quit_activated (GSimpleAction *action,
+                GVariant      *parameter,
+                gpointer       app)
+{
+  board_destroy(&game_board);
+
+  return TRUE;
+}
+
+gboolean
+key_to_board (GtkWidget *widget,
+               GdkEvent  *event,
+               gpointer   user_data)
+{
+  int ch;
+  int moved = 0;
+  
+  gdk_event_get_keyval (event, &ch);
+  
+  switch(ch) {
+    case 'h':
+      moved = board_tile_left(&game_board);
+      if (moved) board_add_tile(&game_board);
+      create_board_grid();
+      break;
+    case 'j':
+      moved = board_tile_down(&game_board);
+      if (moved) board_add_tile(&game_board);
+      create_board_grid();
+      break;
+    case 'k':
+      moved = board_tile_up(&game_board);
+      if (moved) board_add_tile(&game_board);
+      create_board_grid();
+      break;
+    case 'l':
+      moved = board_tile_right(&game_board);
+      if (moved) board_add_tile(&game_board);
+      create_board_grid();
+      break;
+    default:
+      break;
+  };
+}
+
+void create_board_grid()
+{
+  int col, row;
+  int tile_value;
+  int cell_index;
+  char msg[80];
+
+  if (board_container!=NULL) {
+    gtk_container_remove(GTK_CONTAINER(window), board_container);
+  }
+
+  board_container = gtk_grid_new();
+  gtk_grid_set_column_homogeneous (GTK_GRID(board_container), TRUE); 
+  gtk_grid_set_row_homogeneous (GTK_GRID(board_container), TRUE); 
+  gtk_container_add(GTK_CONTAINER(window), board_container);
+
+  for (row=0; row<SIZE; row++) {
+    gtk_grid_insert_row(GTK_GRID(board_container), row);
+  }
+  for (col=0; col<SIZE; col++) {
+    gtk_grid_insert_column(GTK_GRID(board_container), col);
+  }
+
+  for (row=0; row<SIZE; row++) {
+    for (col=0; col<SIZE; col++) {
+      cell_index = col + row*SIZE;
+      tile_value = board_get_tile_value(&game_board, cell_index);
+      if (tile_value>0) {
+        sprintf(msg, "%d", tile_value);
+      } else {
+        sprintf(msg, "");
+      }
+      GtkWidget *a_frame = gtk_frame_new (NULL);
+      GtkWidget *a_label = gtk_label_new(msg);
+      gtk_container_add(GTK_CONTAINER(a_frame), GTK_WIDGET(a_label));
+      gtk_grid_attach(GTK_GRID(board_container), a_frame, col, row, 1, 1);
+    }
+  }
+  gtk_widget_show_all (window);
+}
 
 static void
 activate (GtkApplication* app,
           gpointer        user_data)
 {
-  GtkWidget *window;
-
-  int i;
-  int j;
+  board_init(&game_board);
+  board_add_tile(&game_board);
 
   window = gtk_application_window_new (app);
   gtk_window_set_title (GTK_WINDOW (window), "2048");
   gtk_window_set_default_size (GTK_WINDOW (window), 600, 600);
+  g_signal_connect(window, "key_release_event", G_CALLBACK(key_to_board), NULL);
+  g_signal_connect (window, "destroy", G_CALLBACK (quit_activated), NULL);
 
-  GtkWidget *board_container = gtk_grid_new();
-  gtk_grid_set_column_homogeneous ((GtkGrid*)board_container, TRUE); 
-  gtk_grid_set_row_homogeneous ((GtkGrid*)board_container, TRUE); 
-  for (j=0; j<4; j++) {
-    gtk_grid_insert_row((GtkGrid*)board_container, j);
-  }
-  for (i=0; i<4; i++) {
-    gtk_grid_insert_column((GtkGrid*)board_container, i);
-  }
-  gtk_container_add(GTK_CONTAINER(window), board_container);
-
-  for (j=0; j<4; j++) {
-  for (i=0; i<4; i++) {
-    char msg[80];
-    sprintf(msg, "Test %d %d", j, i);
-    GtkWidget *a_label = gtk_label_new(msg);
-    gtk_label_set_xalign((GtkLabel*)a_label, 0.5);
-
-    /* assegnare gli handler */
-
-    /* assegnare qui gli elementi al container */
-    gtk_grid_attach((GtkGrid*)board_container, a_label, i, j, 1, 1);
-  }
-  }
+  create_board_grid();
 
   gtk_widget_show_all (window);
 }
+
 
 int
 main (int    argc,
