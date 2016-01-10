2e49059ad598b1bd24edb9e6bd26fe37a03f6a82
FabioZTessitore <fabioztessitore@gmail.com>
2016-01-10 19:07:52 +0100

    restored main_console.c

--- main_console.c
+++ main_console.c
@@ -1,48 +1,34 @@
 /* 2048 */
 
-/* versione ncurses */
+/* versione console */
 
-#include <ncurses.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "board.h"
 
 #define UP 1
 #define DOWN 2
 #define LEFT 3
 #define RIGHT 4
-#define HALT 5
 
-void welcome();
-void stampa_lato_orizzontale(int, int, int, int);
-void stampa_scacchiera(Board *b);
+void clear_screen();
 int user_choice();
+
+void stampa_scacchiera(Board *b);
+void stampa_lato_orizzontale(int, int);
 
 int main()
 {
   Board game_board;
   int direction;
   int moved = 0;
-  int halted = 0;
 
-  initscr();
-  noecho();
-  curs_set(0);
-  raw();
-  keypad(stdscr, TRUE);
-  start_color();
+  printf("\n\n\n2048 ... il gioco!\n");
+  printf("******************\n\n");
 
-  init_pair(1, COLOR_BLACK, COLOR_WHITE);
-  init_pair(2, COLOR_BLACK, COLOR_CYAN);
-  init_pair(3, COLOR_BLACK, COLOR_BLUE);
-  init_pair(4, COLOR_BLACK, COLOR_GREEN);
-  init_pair(5, COLOR_BLACK, COLOR_YELLOW);
-  init_pair(6, COLOR_BLACK, COLOR_RED);
 
   board_init(&game_board);
   board_add_tile(&game_board);
-
-  welcome();
 
   do {
     board_add_tile(&game_board);
@@ -63,168 +49,101 @@ int main()
         case RIGHT:
           moved = board_tile_right(&game_board);
           break;
-        case HALT:
-          halted = 1;
-          break;
         default:
           fprintf(stderr, "input non valido, non avrebbe dovuto succedere\n");
           exit(-1);
           break;
       };
-      clear();
-    } while (!moved && !halted);
-  } while(board_some_cell_empty(&game_board) && !halted);
+      clear_screen();
+    } while (!moved);
+  } while(board_some_cell_empty(&game_board));
 
+  printf("\n\n\nGioco Finito!\n");
+  printf("*************\n\n");
+  
   board_destroy(&game_board);
-
-  endwin();
 
   return 0;
 }
 
-void welcome()
-{
-  int row = 5;
-  int col = 0;
-  char *msg;
-  int msg_len;
-
-  msg = "2048 - Il Gioco";
-  msg_len = strlen(msg);
-  col = (COLS-msg_len)/2;
-  mvprintw(row, col, msg);
-
-  msg_len = 8;
-  col = (COLS-msg_len)/2;
-  msg = "h - LEFT";
-  mvprintw(row+2, col, msg);
-  msg = "j - DOWN";
-  mvprintw(row+3, col, msg);
-  msg = "k - UP";
-  mvprintw(row+4, col, msg);
-  msg = "l - RIGHT";
-  mvprintw(row+5, col, msg);
-
-  msg = "(Premi un tasto per iniziare)";
-  msg_len = strlen(msg);
-  col = (COLS-msg_len)/2;
-  mvprintw(row+8, col, msg);
-
-  refresh();
-  getch();
-  clear();
-}
-
-void stampa_lato_orizzontale(int row, int col, int n_celle, int size_cella)
-{
-  int i, j;
-
-
-  for (j=0; j<n_celle; j++) {
-    mvaddch(row, col++, '+');
-    for (i=0; i<size_cella; i++) mvaddch(row, col++, '-');
+void clear_screen() {
+  int i = 35;
+  for (; i>0; i--) {
+    putchar('\n');
   }
-  mvaddch(row, col++, '+');
-}
-
-void stampa_scacchiera(Board *b)
-{
-  int i, j;
-  int cell_index;
-  int tile_value;
-  const int size_cella = 4;
-  int row, col;
-  int cod_color = 1;
-
-  row = 0;
-  col = (COLS-SIZE*size_cella-5)/2;
-
-  for (i=0; i<SIZE; i++) {
-    col = (COLS-SIZE*size_cella-5)/2;
-    
-    stampa_lato_orizzontale(row, col, SIZE, size_cella);
-    row++;
-
-    for (j=0; j<SIZE; j++) {
-      cell_index = j + i*SIZE;
-      tile_value = board_get_tile_value(b, cell_index);
-      if (tile_value>0) {
-        mvaddch(row, col++, '|');
-        if (tile_value >= 1024) {
-          cod_color = 6;
-        } else if (tile_value >= 256) {
-          cod_color = 5;
-        } else if (tile_value >= 64) {
-          cod_color = 4;
-        } else if (tile_value >= 16) {
-          cod_color = 3;
-        } else if (tile_value >= 8) {
-          cod_color = 2;
-        } else {
-          cod_color = 1;
-        }
-        attron(COLOR_PAIR(cod_color));
-        mvprintw(row, col, "%4d", tile_value);
-        attroff(COLOR_PAIR(cod_color));
-        col += 4;
-      } else {
-        mvprintw(row, col, "|    ");
-        col += 5;
-      }
-    }
-    mvprintw(row, col, "|");
-    row++;
-
-  }
-  col = (COLS-SIZE*size_cella-5)/2;
-  stampa_lato_orizzontale(row, col, SIZE, size_cella);
-  row++;
-
-  refresh();
 }
 
 int user_choice()
 {
-  int c;
-  char *msg;
-  int msg_len;
-  int col;
+  int ch, temp;
 
   do {
-    msg = "(h - LEFT   j - DOWN   k - UP   l - RIGHT)";
-    msg_len = strlen(msg);
-    col = (COLS-msg_len)/2;
-    mvprintw(LINES-3, col, msg);
+    printf("\n\nh LEFT, j DOWN, k UP, l RIGHT\n? ");
+    while ( (ch=getchar())!='\n' && ch!='h' && ch!='j' && ch!='k' && ch!='l' )
+      ;
 
-    msg = "<<< F2 per uscire >>>";
-    msg_len = strlen(msg);
-    col = (COLS-msg_len)/2;
-    mvprintw(LINES-2, col, msg);
+    if (ch!='\n') {
+      while((temp=getchar())!='\n')
+        ;
+    }
+  } while (ch!='h' && ch!='j' && ch!='k' && ch!='l');
 
-    refresh();
+  switch(ch) {
+    case 'j':
+      ch = DOWN;
+      break;
+    case 'k':
+      ch = UP;
+      break;
+    case 'h':
+      ch = LEFT;
+      break;
+    case 'l':
+      ch = RIGHT;
+      break;
+    default:
+      ch = 0;
+  };
 
-    c = getch();
+  return ch;
+}
 
-    switch(c) {
-      case 'j':
-        c = DOWN;
-        break;
-      case 'k':
-        c = UP;
-        break;
-      case 'h':
-        c = LEFT;
-        break;
-      case 'l':
-        c = RIGHT;
-        break;
-      case KEY_F(2):
-        c = HALT;
-        break;
-      default:
-        c = 0;
-    };
-  } while (c==0);
+void stampa_scacchiera(Board *b)
+{
+  int row, col;
+  int cell_index;
+  int tile_value;
+  const int size_cella = 4;
 
-  return c;
+  for (row=0; row<SIZE; row++) {
+    
+    stampa_lato_orizzontale(SIZE, size_cella);
+
+    printf("   ");
+    for (col=0; col<SIZE; col++) {
+      cell_index = col + row*SIZE;
+      tile_value = board_get_tile_value(b, cell_index);
+      if (tile_value>0) {
+        printf("|%4d", tile_value);
+      } else {
+        printf("|    ");
+      }
+    }
+    printf("|\n");
+
+  }
+  stampa_lato_orizzontale(SIZE, size_cella);
+  putchar('\n');
+}
+void stampa_lato_orizzontale(int n_celle, int size_cella)
+{
+  int i, j;
+
+  printf("   ");
+  for (j=0; j<n_celle; j++) {
+    putchar('+');
+    for (i=0; i<size_cella; i++) putchar('-');
+  }
+  putchar('+');
+  putchar('\n');
 }
